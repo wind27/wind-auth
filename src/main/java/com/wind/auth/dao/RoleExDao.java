a8679bf971f536417cc1e99d39833194a11a2d19
qianchun <qianchun@huli.com>
2019-03-15 17:57:46 +0800

    test

---
+++ src/main/java/com/wind/auth/dao/RoleExDao.java
@@ -0,0 +1,12 @@
+package com.wind.auth.dao;
+
+/**
+ * PermissionExDao
+ *
+ * @author qianchun
+ * @date 2019/3/12
+ **/
+public interface RoleExDao extends RoleDao {
+
+
+}
---
+++ src/main/java/com/wind/auth/dao/UserExDao.java
@@ -0,0 +1,22 @@
+package com.wind.auth.dao;
+
+import com.wind.auth.model.User;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectProvider;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * PermissionExDao
+ *
+ * @author qianchun
+ * @date 2019/3/12
+ **/
+public interface UserExDao extends UserDao {
+    @Select(SELECT_SQL + " WHERE mobile = #{mobile} limit 0, 1")
+    User findByMobile(String mobile);
+
+    @SelectProvider(type = UserProvider.class, method = "findPage")
+    List<User> findPage(Map<String, Object> params);
+}
